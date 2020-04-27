09147f91da51a38620d03df22a0dc74e66146505
yuto.tsuchiya <tsuchiya0410yuto@gmail.com>
2020-04-27 08:25:45 +0000

    fix migrate

--- db/migrate/20200426093959_sakuzyo.rb
+++
@@ -1,5 +0,0 @@
-class Sakuzyo < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :microposts
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_093959) do
+ActiveRecord::Schema.define(version: 2020_04_26_083944) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
